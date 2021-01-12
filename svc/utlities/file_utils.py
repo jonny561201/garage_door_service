b7584182bd2f43db9c8bef7983022468fa8f3245
Jon Graf <jongraf1@gmail.com>
2021-01-11 23:09:40 -0600

    added method to write json content to file if it exits or not

--- svc/utlities/file_utils.py
+++ svc/utlities/file_utils.py
@@ -1,2 +1,15 @@
+import json
+
+
 def write_status_to_file(door_number, time):
-    pass
+    try:
+        with open('data.json', 'r', encoding='utf-8') as file:
+            content = json.load(file)
+            content[door_number] = f'{time:%Y-%m-%d %H:%M:%S%z}'
+
+        with open('data.json', "w") as file:
+            json.dump(content, file)
+    except FileNotFoundError:
+        content = {door_number: f'{time:%Y-%m-%d %H:%M:%S%z}'}
+        with open('data.json', "w+") as file:
+            json.dump(content, file)
