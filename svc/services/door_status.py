29a0ec5ca5183bf11cb1bac1e9e52b28d2556fe5
Jon Graf <jongraf1@gmail.com>
2021-01-11 21:59:45 -0600

    updated the monitor method to check the status of both doors and record it to the state object

--- svc/services/door_status.py
+++ svc/services/door_status.py
@@ -2,18 +2,31 @@ from datetime import datetime
 
 import pytz
 
+from svc.constants.garage_state import GarageState
 from svc.constants.home_automation import Automation
 from svc.utlities.gpio_utils import is_garage_open
 
 
-def monitor_status(state, garage_id):
-    status = is_garage_open(garage_id)
+def monitor_status():
+    state = GarageState.get_instance()
+    first_status = is_garage_open('1')
+    second_status = is_garage_open('2')
 
-    if status and state.OPEN_TIME is None:
-        state.STATUS = Automation.GARAGE.OPEN
-        state.OPEN_TIME = datetime.now(pytz.utc)
-        state.CLOSED_TIME = None
-    if not status and state.CLOSED_TIME is None:
-        state.STATUS = Automation.GARAGE.CLOSED
-        state.CLOSED_TIME = datetime.now(pytz.utc)
-        state.OPEN_TIME = None
+    first_door = state.DOORS.get('1')
+    second_door = state.DOORS.get('2')
+    if first_status and first_door.OPEN_TIME is None:
+        first_door.STATUS = Automation.GARAGE.OPEN
+        first_door.OPEN_TIME = datetime.now(pytz.utc)
+        first_door.CLOSED_TIME = None
+    if not first_status and first_door.CLOSED_TIME is None:
+        first_door.STATUS = Automation.GARAGE.CLOSED
+        first_door.CLOSED_TIME = datetime.now(pytz.utc)
+        first_door.OPEN_TIME = None
+    if second_status and second_door.OPEN_TIME is None:
+        second_door.STATUS = Automation.GARAGE.OPEN
+        second_door.OPEN_TIME = datetime.now(pytz.utc)
+        second_door.CLOSED_TIME = None
+    if not second_status and second_door.CLOSED_TIME is None:
+        second_door.STATUS = Automation.GARAGE.CLOSED
+        second_door.CLOSED_TIME = datetime.now(pytz.utc)
+        second_door.OPEN_TIME = None
--- test/unit/services/test_door_status.py
+++ test/unit/services/test_door_status.py
@@ -8,84 +8,157 @@ from svc.constants.home_automation import Automation
 from svc.services.door_status import monitor_status
 
 
-@patch('svc.services.garage_door.datetime')
-@patch('svc.services.garage_door.is_garage_open')
+@patch('svc.services.door_status.datetime')
+@patch('svc.services.door_status.is_garage_open')
 class TestGarageService:
 
-    GARAGE_ID = '1'
+    DOOR_ONE = '1'
+    DOOR_TWO = '2'
     DATE = datetime.now(pytz.utc)
-    STATE = GarageState.get_instance().DOORS[GARAGE_ID]
+    STATE = GarageState.get_instance()
 
     def setup_method(self):
-        self.STATE.CLOSED_TIME = None
-        self.STATE.OPEN_TIME = None
-        self.STATE.STATUS = None
+        self.STATE.DOORS[self.DOOR_ONE].CLOSED_TIME = None
+        self.STATE.DOORS[self.DOOR_ONE].OPEN_TIME = None
+        self.STATE.DOORS[self.DOOR_ONE].STATUS = None
+        self.STATE.DOORS[self.DOOR_TWO].CLOSED_TIME = None
+        self.STATE.DOORS[self.DOOR_TWO].OPEN_TIME = None
+        self.STATE.DOORS[self.DOOR_TWO].STATUS = None
 
-    def test_monitor_status__should_call_is_garage_open(self, mock_status, mock_date):
-        monitor_status(self.STATE, self.GARAGE_ID)
+    def test_monitor_status__should_call_is_garage_open_for_first_door(self, mock_status, mock_date):
+        monitor_status()
 
-        mock_status.assert_called_with(self.GARAGE_ID)
+        mock_status.assert_any_call(self.DOOR_ONE)
 
-    def test_monitor_status__should_set_status_to_open_when_garage_open(self, mock_status, mock_date):
+    def test_monitor_status__should_set_status_to_open_when_garage_open_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = True
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.STATUS == Automation.GARAGE.OPEN
+        assert self.STATE.DOORS[self.DOOR_ONE].STATUS == Automation.GARAGE.OPEN
 
-    def test_monitor_status__should_set_open_time_when_garage_open(self, mock_status, mock_date):
+    def test_monitor_status__should_set_open_time_when_garage_open_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = True
         mock_date.now.return_value = self.DATE
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.OPEN_TIME == self.DATE
+        assert self.STATE.DOORS[self.DOOR_ONE].OPEN_TIME == self.DATE
 
-    def test_monitor_status__should_set_status_to_closed_when_garage_door_closed(self, mock_status, mock_date):
+    def test_monitor_status__should_set_status_to_closed_when_garage_door_closed_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = False
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.STATUS == Automation.GARAGE.CLOSED
+        assert self.STATE.DOORS[self.DOOR_ONE].STATUS == Automation.GARAGE.CLOSED
 
-    def test_monitor_status__should_set_closed_time_when_garage_closed(self, mock_status, mock_date):
+    def test_monitor_status__should_set_closed_time_when_garage_closed_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = False
         mock_date.now.return_value = self.DATE
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.CLOSED_TIME == self.DATE
+        assert self.STATE.DOORS[self.DOOR_ONE].CLOSED_TIME == self.DATE
 
-    def test_monitor_status__should_nullify_open_date_when_closed(self, mock_status, mock_date):
+    def test_monitor_status__should_nullify_open_date_when_closed_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = False
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.OPEN_TIME is None
+        assert self.STATE.DOORS[self.DOOR_ONE].OPEN_TIME is None
 
-    def test_monitor_status__should_nullify_closed_date_when_opened(self, mock_status, mock_date):
+    def test_monitor_status__should_nullify_closed_date_when_opened_for_first_door(self, mock_status, mock_date):
         mock_status.return_value = True
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.CLOSED_TIME is None
+        assert self.STATE.DOORS[self.DOOR_ONE].CLOSED_TIME is None
 
-    def test_monitor_status__should_not_reset_open_date_when_already_open(self, mock_status, mock_date):
+    def test_monitor_status__should_not_reset_open_date_when_already_open_for_first_door(self, mock_status, mock_date):
         older_date = datetime.now() - timedelta(days=1)
         mock_status.return_value = True
         mock_date.now.return_value = self.DATE
-        self.STATE.OPEN_TIME = older_date
+        self.STATE.DOORS[self.DOOR_ONE].OPEN_TIME = older_date
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.OPEN_TIME == older_date
+        assert self.STATE.DOORS[self.DOOR_ONE].OPEN_TIME == older_date
 
-    def test_monitor_status__should_not_reset_closed_date_when_already_closed(self, mock_status, mock_date):
+    def test_monitor_status__should_not_reset_closed_date_when_already_closed_for_first_door(self, mock_status, mock_date):
         older_date = datetime.now() - timedelta(days=1)
         mock_status.return_value = False
         mock_date.now.return_value = self.DATE
-        self.STATE.CLOSED_TIME = older_date
+        self.STATE.DOORS[self.DOOR_ONE].CLOSED_TIME = older_date
 
-        monitor_status(self.STATE, self.GARAGE_ID)
+        monitor_status()
 
-        assert self.STATE.CLOSED_TIME == older_date
+        assert self.STATE.DOORS[self.DOOR_ONE].CLOSED_TIME == older_date
+
+    def test_monitor_status__should_call_is_garage_open_for_second_door(self, mock_status, mock_date):
+        monitor_status()
+
+        mock_status.assert_any_call(self.DOOR_TWO)
+
+    def test_monitor_status__should_set_status_to_open_when_garage_open_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = True
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].STATUS == Automation.GARAGE.OPEN
+
+    def test_monitor_status__should_set_open_time_when_garage_open_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = True
+        mock_date.now.side_effect = [self.DATE, self.DATE]
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].OPEN_TIME == self.DATE
+
+    def test_monitor_status__should_set_status_to_closed_when_garage_door_closed_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = False
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].STATUS == Automation.GARAGE.CLOSED
+
+    def test_monitor_status__should_set_closed_time_when_garage_closed_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = False
+        mock_date.now.return_value = self.DATE
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].CLOSED_TIME == self.DATE
+
+    def test_monitor_status__should_nullify_open_date_when_closed_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = False
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].OPEN_TIME is None
+
+    def test_monitor_status__should_nullify_closed_date_when_opened_for_second_door(self, mock_status, mock_date):
+        mock_status.return_value = True
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].CLOSED_TIME is None
+
+    def test_monitor_status__should_not_reset_open_date_when_already_open_for_second_door(self, mock_status, mock_date):
+        older_date = datetime.now() - timedelta(days=1)
+        mock_status.return_value = True
+        mock_date.now.return_value = self.DATE
+        self.STATE.DOORS[self.DOOR_TWO].OPEN_TIME = older_date
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].OPEN_TIME == older_date
+
+    def test_monitor_status__should_not_reset_closed_date_when_already_closed_for_second_door(self, mock_status, mock_date):
+        older_date = datetime.now() - timedelta(days=1)
+        mock_status.return_value = False
+        mock_date.now.return_value = self.DATE
+        self.STATE.DOORS[self.DOOR_TWO].CLOSED_TIME = older_date
+
+        monitor_status()
+
+        assert self.STATE.DOORS[self.DOOR_TWO].CLOSED_TIME == older_date
