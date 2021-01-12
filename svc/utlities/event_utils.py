64eaec506344b21d8f172c14cde9a3d172ca817f
Jon Graf <jongraf1@gmail.com>
2021-01-11 18:02:54 -0600

    added event utility to create a worker thread

---
+++ svc/utlities/event_utils.py
@@ -0,0 +1,22 @@
+from threading import Thread, Event
+
+
+class MyThread(Thread):
+    def __init__(self, event, sched_function, function_interval):
+        Thread.__init__(self)
+        self.stopped = event
+        self.function = sched_function
+        self.interval = function_interval
+
+    def run(self):
+        while not self.stopped.wait(self.interval):
+            self.function()
+
+
+# stop_event.set() will kill the process
+def create_thread(state, fn, delay=5):
+    stop_event = Event()
+    state.STOP_EVENT = stop_event
+    fn()
+    state.ACTIVE_THREAD = MyThread(stop_event, fn, delay)
+    state.ACTIVE_THREAD.start()
