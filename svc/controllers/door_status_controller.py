ab6e4f4bbb35045eca9f09e7488a589baffaa6c7
Jon Graf <jongraf1@gmail.com>
2021-01-17 16:38:49 -0600

    removed state parameter accidentally being passed into the create thread function

--- svc/controllers/door_status_controller.py
+++ svc/controllers/door_status_controller.py
@@ -6,5 +6,5 @@ from svc.utilities.event_utils import create_thread
 def create_status_app():
     state = GarageState.get_instance()
     if state.ACTIVE_THREAD is None:
-        state.ACTIVE_THREAD = create_thread(state, monitor_status)
+        state.ACTIVE_THREAD = create_thread(monitor_status)
         state.ACTIVE_THREAD.start()
